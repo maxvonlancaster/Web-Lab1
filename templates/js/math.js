81d5a9b4254c77d6483792d52dbfda30f0a87831
Ivan <example@gmail.com>
2023-02-02 11:43:30 +0200

    Add check for numbers

--- templates/js/math.js
+++ templates/js/math.js
@@ -1,13 +1,21 @@
 function multi() {
     let num1, num2, result, result2;
-    num1 = document.getElementById('inp_1').value;
+    num1 = document.getElementById("inp_1").value;
     num1 = parseInt(num1);
 
-    num2 = document.getElementById('inp_2').value;
+    num2 = document.getElementById("inp_2").value;
     num2 = parseInt(num2);
 
-    result = num1 * num2;
-    document.getElementById('result_multi').innerHTML = result;
-    result2 = num1 + num2
-    document.getElementById('result_sum').innerHTML = result2;
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        result = num1 * num2;
+        document.getElementById("result_multi").innerHTML = result;
+        result2 = num1 + num2;
+        document.getElementById("result_sum").innerHTML = result2;
+    }
+    else{
+        result = "Помилка введіть число";
+        document.getElementById("result_multi").innerHTML = result;
+        result2 = "Помилка введіть число";
+        document.getElementById("result_sum").innerHTML = result2;
+    }
 }
